95d53a1f7c5413557ebea004ef0d8179e50c9c9d
Guilherme Gomes <148831147+DevGuilhermeg@users.noreply.github.com>
2023-11-23 22:30:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://cadastro_usuario_user:ZEeEUZKLOE21ZUprGPLy1rWlmD0S9DLx@dpg-cleo2lbl00ks739s9660-a/cadastro_usuario'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)
+
 db = SQLAlchemy(app)
 
 class Pessoa(db.Model):
@@ -11,22 +15,38 @@ class Pessoa(db.Model):
     email = db.Column(db.String(50), unique=True, nullable=False)
     idade = db.Column(db.Integer, nullable=False)
 
-# Cria as tabelas no banco de dados
-with app.app_context():
-    db.create_all()
+@app.route('/pessoas', methods=['GET'])
+def obter_pessoas():
+    pessoas = Pessoa.query.all()
+    if not pessoas:
+        return jsonify({"mensagem": "Nenhuma pessoa cadastrada."})
+    
+    pessoas_json = [{"id": pessoa.id, "nome": pessoa.nome, "email": pessoa.email, "idade": pessoa.idade} for pessoa in pessoas]
+    return jsonify({"pessoas": pessoas_json})
 
-@app.route('/teste-conexao-bd', methods=['GET'])
-def teste_conexao_bd():
-    try:
-        # Adiciona uma pessoa ao banco de dados
-        nova_pessoa = Pessoa(nome="Exemplo", email="exemplo@email.com", idade=25)
-        db.session.add(nova_pessoa)
+
+@app.route('/pessoas', methods=['POST'])
+def cadastrar_pessoa():
+    dados = request.json
+    if not all(key in dados for key in ['nome', 'email', 'idade']):
+        return jsonify({"erro": "Campos obrigatórios ausentes"}), 400
+
+    nova_pessoa = Pessoa(nome=dados['nome'], email=dados['email'], idade=dados['idade'])
+    db.session.add(nova_pessoa)
+    db.session.commit()
+    return jsonify({"mensagem": "Pessoa cadastrada com sucesso!"})
+    
+@app.route('/pessoas/<int:pessoa_id>', methods=['DELETE'])
+def excluir_pessoa(pessoa_id):
+    pessoa = Pessoa.query.get(pessoa_id)
+    if pessoa:
+        db.session.delete(pessoa)
         db.session.commit()
-
-        return jsonify({"mensagem": "Conexão com o banco de dados bem-sucedida e registro adicionado."})
-    except Exception as e:
-        return jsonify({"erro": f"Erro na conexão com o banco de dados: {str(e)}"}), 500
+        return jsonify({"mensagem": f"Pessoa com ID {pessoa_id} excluída com sucesso!"})
+    else:
+        return jsonify({"erro": f"Pessoa com ID {pessoa_id} não encontrada"}), 404
 
 if __name__ == '__main__':
-    # Inicia o servidor Flask em modo de depuração
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()  # Cria as tabelas no banco de dados
+    app.run(debug=True)  # Inicia o servidor Flask em modo de depuração
