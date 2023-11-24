5fc991da1bf937e31c72529a506b0481f4d67825
Guilherme Gomes <148831147+DevGuilhermeg@users.noreply.github.com>
2023-11-24 19:27:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_bcrypt import Bcrypt  # Importe a extensão Flask-Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://bando_usuarios_user:FQx4koA5p10ddjvfgvs5A6pwTWkp8UNF@dpg-clgi4t6g1b2c73a8ueq0-a/bando_usuarios'
@@ -9,14 +8,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)
 
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)  # Crie uma instância do Flask-Bcrypt
 
 class Pessoa(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     idade = db.Column(db.Integer, nullable=False)
-    senha = db.Column(db.String(60), nullable=False)
 
 @app.route('/pessoas', methods=['GET'])
 def obter_pessoas():
@@ -27,19 +24,18 @@ def obter_pessoas():
     pessoas_json = [{"id": pessoa.id, "nome": pessoa.nome, "email": pessoa.email, "idade": pessoa.idade} for pessoa in pessoas]
     return jsonify({"pessoas": pessoas_json})
 
+
 @app.route('/pessoas', methods=['POST'])
 def cadastrar_pessoa():
     dados = request.json
-    if not all(key in dados for key in ['nome', 'email', 'idade', 'senha']):
+    if not all(key in dados for key in ['nome', 'email', 'idade']):
         return jsonify({"erro": "Campos obrigatórios ausentes"}), 400
 
-    senha_hash = bcrypt.generate_password_hash(dados['senha']).decode('utf-8')
-
-    nova_pessoa = Pessoa(nome=dados['nome'], email=dados['email'], idade=dados['idade'], senha=senha_hash)
+    nova_pessoa = Pessoa(nome=dados['nome'], email=dados['email'], idade=dados['idade'])
     db.session.add(nova_pessoa)
     db.session.commit()
     return jsonify({"mensagem": "Pessoa cadastrada com sucesso!"})
-
+    
 @app.route('/pessoas/<int:pessoa_id>', methods=['DELETE'])
 def excluir_pessoa(pessoa_id):
     pessoa = Pessoa.query.get(pessoa_id)
@@ -52,5 +48,5 @@ def excluir_pessoa(pessoa_id):
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+        db.create_all()  # Cria as tabelas no banco de dados
+    app.run(debug=True)  # Inicia o servidor Flask em modo de depuração
