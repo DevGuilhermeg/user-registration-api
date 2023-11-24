146f66056b512b17d13641491d1b6f5039f68aed
Guilherme Gomes <148831147+DevGuilhermeg@users.noreply.github.com>
2023-11-23 22:16:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from sqlalchemy import text  # Importe a classe 'text' do SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://cadastro_usuario_user:ZEeEUZKLOE21ZUprGPLy1rWlmD0S9DLx@dpg-cleo2lbl00ks739s9660-a/cadastro_usuario'
@@ -9,11 +9,10 @@ db = SQLAlchemy(app)
 @app.route('/teste-conexao-bd', methods=['GET'])
 def teste_conexao_bd():
     try:
-        result = db.session.execute("SELECT 1")
+        result = db.session.execute(text("SELECT 1", bind=db.engine))
         if result.scalar() == 1:
             return jsonify({"mensagem": "Conexão com o banco de dados bem-sucedida."})
         else:
             return jsonify({"erro": "A consulta não retornou o valor esperado."}), 500
     except Exception as e:
         return jsonify({"erro": f"Erro na conexão com o banco de dados: {str(e)}"}), 500
-
