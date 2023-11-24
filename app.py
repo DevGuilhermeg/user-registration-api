95356ec7db3242ca3160f1cf68b9fd667591ae5f
Guilherme Gomes <148831147+DevGuilhermeg@users.noreply.github.com>
2023-11-24 01:33:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class Pessoa(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
-    senha = db.Column(db.String(60), nullable=False)
+    idade = db.Column(db.Integer, nullable=False)
 
 @app.route('/pessoas', methods=['GET'])
 def obter_pessoas():
