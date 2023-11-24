853ad7d0d1ed189153017e082d52813ac9cc4965
Guilherme Gomes <148831147+DevGuilhermeg@users.noreply.github.com>
2023-11-24 19:40:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://bando_usuarios_user:FQx4koA5p10ddjvfgvs5A6pwTWkp8UNF@dpg-clgi4t6g1b2c73a8ueq0-a/bando_usuarios'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bando_usuarios_user:FQx4koA5p10ddjvfgvs5A6pwTWkp8UNF@dpg-clgi4t6g1b2c73a8ueq0-a/bando_usuarios'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)
 
