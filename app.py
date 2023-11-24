3f84030022b8b825ba94f563ca1a3c3e0eaa1f90
Guilherme Gomes <148831147+DevGuilhermeg@users.noreply.github.com>
2023-11-24 00:22:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ class Pessoa(db.Model):
     nome = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     idade = db.Column(db.Integer, nullable=False)
-    senha = db.Column(db.String(128), nullable=False)  # O comprimento padrão para o hash do Werkzeug
 
 @app.route('/pessoas', methods=['GET'])
 def obter_pessoas():
@@ -26,8 +25,28 @@ def obter_pessoas():
     return jsonify({"pessoas": pessoas_json})
 
 
+@app.route('/pessoas', methods=['POST'])
+def cadastrar_pessoa():
+    dados = request.json
+    if not all(key in dados for key in ['nome', 'email', 'idade']):
+        return jsonify({"erro": "Campos obrigatórios ausentes"}), 400
+
+    nova_pessoa = Pessoa(nome=dados['nome'], email=dados['email'], idade=dados['idade'])
+    db.session.add(nova_pessoa)
+    db.session.commit()
+    return jsonify({"mensagem": "Pessoa cadastrada com sucesso!"})
+    
+@app.route('/pessoas/<int:pessoa_id>', methods=['DELETE'])
+def excluir_pessoa(pessoa_id):
+    pessoa = Pessoa.query.get(pessoa_id)
+    if pessoa:
+        db.session.delete(pessoa)
+        db.session.commit()
+        return jsonify({"mensagem": f"Pessoa com ID {pessoa_id} excluída com sucesso!"})
+    else:
+        return jsonify({"erro": f"Pessoa com ID {pessoa_id} não encontrada"}), 404
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Cria as tabelas no banco de dados
-    app.run(debug=True, use_reloader=False)  # Inicia o servidor Flask em modo de depuração
+    app.run(debug=True)  # Inicia o servidor Flask em modo de depuração
