7f7fcfed5e83ea73b6e59ff1b17b4d220afe58f3
Guilherme Gomes <148831147+DevGuilhermeg@users.noreply.github.com>
2023-11-23 22:03:44 -0300

    Update23 app.py

--- app.py
+++ app.py
@@ -9,45 +9,11 @@ CORS(app)
 
 db = SQLAlchemy(app)
 
-class Pessoa(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    idade = db.Column(db.Integer, nullable=False)
-
-@app.route('/pessoas', methods=['GET'])
-def obter_pessoas():
-    pessoas = Pessoa.query.all()
-    if not pessoas:
-        return jsonify({"mensagem": "Nenhuma pessoa cadastrada."})
-    
-    pessoas_json = [{"id": pessoa.id, "nome": pessoa.nome, "email": pessoa.email, "idade": pessoa.idade} for pessoa in pessoas]
-    return jsonify({"pessoas": pessoas_json})
-
-
-@app.route('/pessoas', methods=['POST'])
-def cadastrar_pessoa():
-    dados = request.json
-    if not all(key in dados for key in ['nome', 'email', 'idade']):
-        return jsonify({"erro": "Campos obrigatórios ausentes"}), 400
-
-    nova_pessoa = Pessoa(nome=dados['nome'], email=dados['email'], idade=dados['idade'])
-    db.session.add(nova_pessoa)
-    db.session.commit()
-    return jsonify({"mensagem": "Pessoa cadastrada com sucesso!"})
-    
-@app.route('/pessoas/<int:pessoa_id>', methods=['DELETE'])
-def excluir_pessoa(pessoa_id):
-    pessoa = Pessoa.query.get(pessoa_id)
-    if pessoa:
-        db.session.delete(pessoa)
-        db.session.commit()
-        return jsonify({"mensagem": f"Pessoa com ID {pessoa_id} excluída com sucesso!"})
-    else:
-        return jsonify({"erro": f"Pessoa com ID {pessoa_id} não encontrada"}), 404
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Cria as tabelas no banco de dados
-    app.run(debug=True)  # Inicia o servidor Flask em modo de depuração
+@app.route('/teste-conexao-bd', methods=['GET'])
+def teste_conexao_bd():
+    try:
+        db.session.query("1").from_statement("SELECT 1").all()
+        return jsonify({"mensagem": "Conexão com o banco de dados bem-sucedida."})
+    except Exception as e:
+        return jsonify({"erro": f"Erro na conexão com o banco de dados: {str(e)}"}), 500
 
