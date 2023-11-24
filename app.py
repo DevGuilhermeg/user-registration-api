fcbbed8b149e49977d5873852579c0cbb380e99e
Guilherme Gomes <148831147+DevGuilhermeg@users.noreply.github.com>
2023-11-23 22:07:04 -0300

    Update3 app.py

--- app.py
+++ app.py
@@ -4,15 +4,12 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://cadastro_usuario_user:ZEeEUZKLOE21ZUprGPLy1rWlmD0S9DLx@dpg-cleo2lbl00ks739s9660-a/cadastro_usuario'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-CORS(app)
-
 db = SQLAlchemy(app)
 
 @app.route('/teste-conexao-bd', methods=['GET'])
 def teste_conexao_bd():
     try:
-        db.session.query("1").from_statement("SELECT 1").all()
+        db.session.execute("SELECT 1").scalar()
         return jsonify({"mensagem": "Conexão com o banco de dados bem-sucedida."})
     except Exception as e:
         return jsonify({"erro": f"Erro na conexão com o banco de dados: {str(e)}"}), 500
