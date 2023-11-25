7f1e9ab2580da1bd7ba01ba4e821098ed4fc143b
Guilherme Gomes <148831147+DevGuilhermeg@users.noreply.github.com>
2023-11-24 21:07:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ class Pessoa(db.Model):
     nome = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     idade = db.Column(db.Integer, nullable=False)
+    senha = db.Column(db.String(60), nullable=False)
+
+with app.app_context():
+        db.create_all()  # Cria as tabelas no banco de dados
 
 @app.route('/pessoas', methods=['GET'])
 def obter_pessoas():
