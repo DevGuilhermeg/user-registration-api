c04814f7dc08ff7d15b28468089a6c2703957ac9
Guilherme Gomes <148831147+DevGuilhermeg@users.noreply.github.com>
2023-11-24 21:10:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,6 @@ class Pessoa(db.Model):
     idade = db.Column(db.Integer, nullable=False)
     senha = db.Column(db.String(60), nullable=False)
 
-with app.app_context():
-        db.create_all()  # Cria as tabelas no banco de dados
-
 @app.route('/pessoas', methods=['GET'])
 def obter_pessoas():
     pessoas = Pessoa.query.all()
@@ -29,18 +26,19 @@ def obter_pessoas():
     pessoas_json = [{"id": pessoa.id, "nome": pessoa.nome, "email": pessoa.email, "idade": pessoa.idade} for pessoa in pessoas]
     return jsonify({"pessoas": pessoas_json})
 
-
 @app.route('/pessoas', methods=['POST'])
 def cadastrar_pessoa():
     dados = request.json
-    if not all(key in dados for key in ['nome', 'email', 'idade']):
+    if not all(key in dados for key in ['nome', 'email', 'idade', 'senha']):
         return jsonify({"erro": "Campos obrigatórios ausentes"}), 400
 
-    nova_pessoa = Pessoa(nome=dados['nome'], email=dados['email'], idade=dados['idade'])
+    senha_hash = bcrypt.generate_password_hash(dados['senha']).decode('utf-8')
+
+    nova_pessoa = Pessoa(nome=dados['nome'], email=dados['email'], idade=dados['idade'], senha=senha_hash)
     db.session.add(nova_pessoa)
     db.session.commit()
     return jsonify({"mensagem": "Pessoa cadastrada com sucesso!"})
-    
+
 @app.route('/pessoas/<int:pessoa_id>', methods=['DELETE'])
 def excluir_pessoa(pessoa_id):
     pessoa = Pessoa.query.get(pessoa_id)
@@ -51,7 +49,20 @@ def excluir_pessoa(pessoa_id):
     else:
         return jsonify({"erro": f"Pessoa com ID {pessoa_id} não encontrada"}), 404
 
+@app.route('/login', methods=['POST'])
+def login():
+    dados = request.json
+    if not all(key in dados for key in ['email', 'senha']):
+        return jsonify({"erro": "Campos obrigatórios ausentes"}), 400
+
+    usuario = Pessoa.query.filter_by(email=dados['email']).first()
+
+    if usuario and bcrypt.check_password_hash(usuario.senha, dados['senha']):
+        return jsonify({"mensagem": "Login bem-sucedido!"})
+    else:
+        return jsonify({"erro": "Credenciais inválidas"}), 401
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Cria as tabelas no banco de dados
-    app.run(debug=True)  # Inicia o servidor Flask em modo de depuração
+    app.run(debug=True, use_reloader=False)  # Inicia o servidor Flask em modo de depuração
