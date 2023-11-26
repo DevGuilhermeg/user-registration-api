83d88080a1efea6237a8d9982bf24dba75b53fd7
Guilherme Gomes <148831147+DevGuilhermeg@users.noreply.github.com>
2023-11-26 17:54:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,87 +1,88 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask import Flask, render_template, request, redirect
+import requests
+from tkinter import messagebox
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://bando_usuarios_29xx_user:ZaB44taTeXiTopMZsBlU8kmjultHhEqJ@dpg-clgjee58td7s73bi9dkg-a.ohio-postgres.render.com/bando_usuarios_29xx'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-CORS(app)
 
-db = SQLAlchemy(app)
 
-class Pessoa(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    idade = db.Column(db.Integer, nullable=False)
-    senha = db.Column(db.String(60), nullable=False)
+url_pessoas = 'https://api-usuarios-tvdy.onrender.com/pessoas'
+# URL da API para login
+url_login = 'https://api-usuarios-tvdy.onrender.com/login'
+url_cadastrar = 'https://api-usuarios-tvdy.onrender.com/pessoas'
 
-@app.route('/pessoas', methods=['GET'])
-def obter_pessoas():
-    pessoas = Pessoa.query.all()
-    if not pessoas:
-        return jsonify({"mensagem": "Nenhuma pessoa cadastrada."})
+
+@app.route('/')
+def index():
+    # Obtenha o email do parâmetro na URL
+    email = request.args.get('email')
+    dados_usuario = None
+    nome = "Recrutador"
     
-    pessoas_json = [{"id": pessoa.id, "nome": pessoa.nome, "email": pessoa.email, "idade": pessoa.idade} for pessoa in pessoas]
-    return jsonify({"pessoas": pessoas_json})
+    # Se o email estiver presente, faça a solicitação à API para obter dados do usuário, se necessário
+    if email:
+        urlEmail = f'https://api-usuarios-tvdy.onrender.com/pessoas/email/{email}'
+        resposta = requests.get(urlEmail)
 
-@app.route('/pessoas', methods=['POST'])
-def cadastrar_pessoa():
-    try:
-        dados = request.json
-        campos_obrigatorios = ['nome', 'email', 'idade', 'senha']
-        if not all(key in dados for key in campos_obrigatorios):
-            return jsonify({"erro": "Campos obrigatórios ausentes"}), 400
+        if resposta.status_code == 200:
+            # Aqui você pode usar os dados do usuário obtidos da API
+            dados_usuario = resposta.json()
+            nome = (dados_usuario['pessoa']['nome'])
 
-        nova_pessoa = Pessoa(nome=dados['nome'], email=dados['email'], idade=dados['idade'], senha=dados['senha'])
-        db.session.add(nova_pessoa)
-        db.session.commit()
-        return jsonify({"mensagem": "Pessoa cadastrada com sucesso!"})
-
-    except Exception as e:
-        # Adicione um log para registrar a exceção
-        app.logger.error(f"Erro durante o cadastro de pessoa: {str(e)}")
-        return jsonify({"erro": f"Erro interno do servidor: {str(e)}"}), 500
-
-
-@app.route('/pessoas/<int:pessoa_id>', methods=['DELETE'])
-def excluir_pessoa(pessoa_id):
-    pessoa = Pessoa.query.get(pessoa_id)
-    if pessoa:
-        db.session.delete(pessoa)
-        db.session.commit()
-        return jsonify({"mensagem": f"Pessoa com ID {pessoa_id} excluída com sucesso!"})
-    else:
-        return jsonify({"erro": f"Pessoa com ID {pessoa_id} não encontrada"}), 404
-
-@app.route('/pessoas/email/<string:email>', methods=['GET'])
-def obter_pessoa_por_email(email):
-    pessoa = Pessoa.query.filter_by(email=email).first()
+            # ...
     
-    if pessoa:
-        pessoa_json = {"id": pessoa.id, "nome": pessoa.nome, "email": pessoa.email, "idade": pessoa.idade}
-        return jsonify({"pessoa": pessoa_json})
-    else:
-        return jsonify({"mensagem": f"Nenhuma pessoa encontrada com o email {email}"}), 404
+    # Restante do código da rota /index
+    
+    saldaçao = f"Olá, {nome}! Guilherme aqui"
+    print(saldaçao)
+    
+    return render_template('index.html', nome=saldaçao)
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-@app.route('/login', methods=['POST'])
+@app.route('/fazer-login', methods=['POST'])
 def fazer_login():
-    dados = request.json
-    email = dados.get('email')
-    senha = dados.get('senha')
+    # Obter os valores do formulário
+    email = request.form.get('email')
+    senha = request.form.get('senha')
 
-    if not all([email, senha]):
-        return jsonify({"erro": "Campos obrigatórios ausentes"}), 400
+    # Dados de login em formato JSON
+    dados_login = {'email': email, 'senha': senha}
 
-    pessoa = Pessoa.query.filter_by(email=email, senha=senha).first()
+    # Fazendo a requisição POST para o endpoint de login com os dados em JSON
+    resposta = requests.post(url_login, json=dados_login, verify=True)  # O parâmetro 'verify=True' verifica o certificado SSL
 
-    if pessoa:
-        return jsonify({"mensagem": "Login bem-sucedido!"})
+    # Verificando o status da resposta
+    if resposta.status_code == 200:
+        # Redirecionar para a página principal ou outra página após o login bem-sucedido
+        return redirect(f'/?email={email}')
     else:
-        return jsonify({"erro": "Credenciais inválidas"}), 401
+        return f"Falha no login. Código de status: {resposta.status_code}"
+    
 
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/cadastrar', methods=['POST'])
+def cadastrar():
+    # Obter os valores do formulário
+    nome = request.form.get('nameC')
+    email = request.form.get('emailC')
+    idade = request.form.get("idadeC")
+    senha = request.form.get('senhaC')
+
+    dados = {"nome": nome, "email": email, "idade": int(idade), "senha": senha}
+    resposta = requests.post(url_cadastrar, json=dados)
+
+    if resposta.status_code == 200:
+        mensagem = "Pessoa cadastrada com sucesso!"
+        # Retornar a mensagem de sucesso para ser renderizada na página HTML
+        return render_template('cadastro.html', mensagem=mensagem)
+    else:
+        mensagem = "Erro ao cadastrar pessoa."
+        # Retornar a mensagem de erro para ser renderizada na página HTML
+        return render_template('cadastro.html', mensagem_erro=mensagem)
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True)
